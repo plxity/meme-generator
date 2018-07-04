75837162bf0bbad1fbcf83d56244980e0a79e3f7
Apoorv Taneja <purutaneja.com@gmail.com>
2018-07-04 13:43:41 +0530

    Update index.js

--- src/action/index.js
+++ src/action/index.js
@@ -1,4 +1,6 @@
+import {username,password} from './secrets';
 export const EXPORT_MEMES ='get_memes';
+export const NEW_MEME='NEW_MEME';
 function recmeme(json){
     const {memes}=json.data;
     return{
@@ -14,5 +16,34 @@ export function fetchJson(){
     return function(dispatch){
         return fetchmemes()
         .then(json=>dispatch(recmeme(json)))
+    }
+}
+export function newMeme(meme){
+    return {
+        type:NEW_MEME,
+        meme
+    }
+
+}
+function postMemeJson(params){
+    params["username"]=username;
+    params["password"]=password;
+    const bodyParams =Object.keys(params).map(key=>{
+        return encodeURIComponent(key) + '=' +encodeURIComponent(params[key])
+    }).join('&');
+
+    return fetch('https://api.imgflip.com/caption_image',{
+        method: "POST",
+        headers:{
+            'Content-Type':'application/x-www-form-urlencoded'
+        },
+        body: bodyParams
+
+    }).then(response=>response.json());
+}
+export function createMeme(new_object){
+    return function(dispatch){
+        return postMemeJson(new_object)
+        .then(newmeme=>dispatch(newMeme(newmeme)))
     }
 }
